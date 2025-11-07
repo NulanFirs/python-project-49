91b15b6f42e32c4ccae9f3e7eecd2106a104f4e7
nulan firs <nulanfirs@gmail.com>
2025-11-07 13:59:56 +0300

    Шаг7.Игра НОД - выполнен

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from ..engine import run_game
+from ..games.brain_gcd import GAME_DESCRIPTION, generate_round
+
+
+def main():
+    run_game(generate_round, GAME_DESCRIPTION)
+
+
+if __name__ == '__main__':
+    main()
