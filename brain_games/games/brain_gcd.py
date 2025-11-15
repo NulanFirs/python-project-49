b38bf6bf187aa5413a76c2f17b5e1269676e6699
Unya <nulanfirs@gmail.com>
2025-11-15 16:23:51 +0300

    feat: Implemented brain-gcd and fixed venv exclusion

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,18 +1,24 @@
-import math
 import random
 
 GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def generate_round():
+def gcd(a, b):
+    while b:
+        temp = b
+        b = a % b
+        a = temp
+    return a
 
+
+def generate_round():
     num1 = random.randint(1, 50)
     num2 = random.randint(1, 20)
-    
+
     question = f"{num1} {num2}"
 
-    result = math.gcd(num1, num2)
-    
+    result = gcd(num1, num2)
+
     correct_answer = str(result)
 
     return question, correct_answer
